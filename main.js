aaa2e544cc9e23e97ea76879b4ec4cc1576a6ca8
Hannah Gunn <hannahgunn97@gmail.com>
2020-03-11 17:40:08 +0000

    Refresh button for quotes
    Functionality to change the quote when the refresh button is pressed

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ function toggleBurger(){
     burger.toggleClass('is-active');
     menu.toggleClass('is-active');
 }
+
+function textRefesh(){
+    let text = ["Quote 1", "Quote 2", "Quote 3", "Quote 4", "Quote 5", "Quote 6"]
+    let index = Math.floor(Math.random() * text.length)
+    let displayTxt = text[index]
+    document.getElementById('quotes').innerHTML = displayTxt
+}
